4a0d342f13fba91a02117d55e1b2b54fedf8f7a2
张威 <294341493@qq.com>
2018-11-24 12:11:28 +0800

    v1.0.2

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 ## Usage
-> plugin config
+> 下载
+```bash
+npm i postcss-imgpost-alioss -D 或 yarn add postcss-imgpost-alioss --dev
+```
+> 使用方法和postcss插件一样
+
+> 配置
 ```javascript
 const options = {
-  // required
-  accessKeyId: 'your ali accessKeyId',
-  accessKeySecret: 'your ali accessKeySecret',
-  region: 'your ali region',
-  bucket: 'your ali bucket',
-  // not required
-  https: true,
-  domain: 'your domain', // eg: 'domain.com/'
-  name: 'your filename' // eg: 'filename/'
+    // required
+    accessKeyId: 'your ali accessKeyId',
+    accessKeySecret: 'your ali accessKeySecret',
+    region: 'your ali region',
+    bucket: 'your ali bucket',
+    // not required
+    https: true,
+    domain: 'your hostname', // eg: 'hostname.com/'
+    name: 'your filename' // eg: 'filename/'
 }
 ```
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getAbsolutePath(dir, relative) {
 
 	const reg = /\.(jpg|jpeg|png|gif|svg|bmp)\b/i
 	if (!reg.test(absolute)) {
-    console.log(`File is not a image: ${absolute}`)
+        console.log(`File is not a image: ${absolute}`)
 		return
 	}
 
@@ -22,42 +22,42 @@ function getAbsolutePath(dir, relative) {
 }
 
 module.exports = postcss.plugin('postcss-imgpost-alioss', options => {
-  return function (css) {
-    const client = new oss({
-      accessKeyId: options.accessKeyId,
-      accessKeySecret: options.accessKeySecret,
-      region: options.region,
-      bucket: options.bucket
-    })
-    css.walkRules(ruler => {
-      ruler.walkDecls(/^background(-image)?$/, decl => {
-        const imageReg = /url\(['\"]?([^)'\"]+)['\"]?\)/
-        const matchValue = imageReg.exec(decl.value)
-        if (!matchValue || matchValue[1].indexOf('data:') === 0) {
-          return
-        }
-
-        const relativePath = matchValue[1]
-        const inputDir = path.dirname(decl.source.input.file)
-        const absolutePath = getAbsolutePath(inputDir, relativePath)
-        if (!absolutePath) return
-
-        const imageData = fs.readFileSync(absolutePath)
-        const urls = relativePath.split(/^\.+\//)
-        let filename = options.name || ''
-        const url = filename + urls[1]
-        let protol = options.https ? 'https://' : 'http://'
-        let cssUrl = options.domain
-          ? `url('${protol + options.domain + url}')`
-          : `url('${protol + options.bucket}.${options.region}.aliyuncs.com/${url}')`
-        decl.value = decl.value.replace(imageReg, cssUrl)
-
-        co(function* () {
-          const result = yield client.put(url, imageData)
-        }).catch(err => {
-          console.log(err)
+    return function (css) {
+        const client = new oss({
+            accessKeyId: options.accessKeyId,
+            accessKeySecret: options.accessKeySecret,
+            region: options.region,
+            bucket: options.bucket
         })
-      })
-    })
-  }
+        css.walkRules(ruler => {
+            ruler.walkDecls(/^background(-image)?$/, decl => {
+                const imageReg = /url\(['\"]?([^)'\"]+)['\"]?\)/
+                const matchValue = imageReg.exec(decl.value)
+                if (!matchValue || matchValue[1].indexOf('data:') === 0) {
+                    return
+                }
+
+                const relativePath = matchValue[1]
+                const inputDir = path.dirname(decl.source.input.file)
+                const absolutePath = getAbsolutePath(inputDir, relativePath)
+                if (!absolutePath) return
+
+                const imageData = fs.readFileSync(absolutePath)
+                const urls = relativePath.split(/^\.+\//)
+                let filename = options.name || ''
+                const url = filename + urls[1]
+                let protol = options.https ? 'https://' : 'http://'
+                let cssUrl = options.domain
+                    ? `url('${protol + options.domain + url}')`
+                    : `url('${protol + options.bucket}.${options.region}.aliyuncs.com/${url}')`
+                decl.value = decl.value.replace(imageReg, cssUrl)
+
+                co(function* () {
+                    const result = yield client.put(url, imageData)
+                }).catch(err => {
+                    console.log(err)
+                })
+            })
+        })
+    }
 })
